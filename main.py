eb640bb0156be97295b8fa00ba71585911241d4b
Radvanszki <gabor@radvanszki.com>
2025-05-11 11:14:04 +0200

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from freqtrade import FreqtradeBot
-from freqtrade.strategy import IStrategy
-
-class MyStrategy(IStrategy):
-    # Strategy settings and methods
-
-if __name__ == "__main__":
-    bot = FreqtradeBot(strategy=MyStrategy)
-    bot.start(
